14b06ea1ac4a8bb5e8e72dc19cc05d15e1544f7d
kngvxx <kiki.puk@op.pl>
2018-04-16 19:04:34 +0200

    petla

--- sudoku/main.cpp
+++ sudoku/main.cpp
@@ -2,8 +2,20 @@
 
 using namespace std;
 
+int tab[4][4];
+int i,j;
+
 int main()
 {
-    cout << "Hello world!" << endl;
+
+   for(i=1;i<=4;i++)
+   {
+       for(j=1;j<=4;j++)
+       {
+           cout<<"* ";
+       }
+       cout<<endl;
+   }
+
     return 0;
 }
